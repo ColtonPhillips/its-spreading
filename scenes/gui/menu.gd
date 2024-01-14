f6740a32de4cdd771caaa89925dfbd889f59eec6
Colton Phillips <coltonjphillips@gmail.com>
2024-01-14 03:29:47 -0800

    fix collision a bit and other stuff

---
+++ scenes/gui/menu.gd
@@ -0,0 +1,27 @@
+#class_name Menu
+extends Control
+
+func pause():
+	get_tree().paused = true
+	self.visible = true
+	
+func unpause():
+	get_tree().paused = false
+	self.visible = false
+	
+func toggle():
+	get_tree().paused = not get_tree().paused
+	self.visible = not self.visible
+
+func _ready():
+	pause()
+
+func _input(event):
+	if Input.is_action_just_pressed("pause"):
+		toggle()
+
+func _on_exit_pressed():
+		get_tree().quit();
+
+func _on_unpause_pressed():
+	pause()
