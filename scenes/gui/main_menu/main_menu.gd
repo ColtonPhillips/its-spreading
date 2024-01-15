56c42187e35588d1ac8909cdfee7bcceba7c3c7e
Colton Phillips <coltonjphillips@gmail.com>
2024-01-14 21:18:17 -0800

    Refactor slightly main menu to use export

--- scenes/gui/main_menu/main_menu.gd
+++ scenes/gui/main_menu/main_menu.gd
@@ -1,12 +1,15 @@
 extends CenterContainer
 
-func _on_start_game_link_pressed():
-	get_tree().change_scene_to_file('res://scenes/world.tscn')
+@export var start_game_scene: Resource
+@export var credits_scene: Resource
 
+func _on_start_game_link_pressed():
+	#get_tree().change_scene_to_file(start_game_scene)
+	get_tree().change_scene_to_packed(start_game_scene)
 
 func _on_credits_link_pressed():
-	get_tree().change_scene_to_file('res://scenes/gui/credits/credits.tscn')
-
+	#get_tree().change_scene_to_file(credits_scene)
+	get_tree().change_scene_to_packed(credits_scene)
 
 func _on_exit_link_pressed():
-		get_tree().quit();
+	get_tree().quit();
