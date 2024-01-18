f5c2418653c4a71bd2c14dd3637df3429b4cef9e
Colton Phillips <coltonjphillips@gmail.com>
2024-01-18 01:49:35 -0800

    add exp bar. fix gui so it kinda works.

---
+++ scenes/components/exp_bar_component/exp_bar_component.gd
@@ -0,0 +1,23 @@
+class_name ExpBarComponent
+extends Node
+
+@onready var texture_progress_bar = $TextureProgressBar
+var playerStatsComponent: PlayerStatsComponent
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	playerStatsComponent = Global.player.stats_component
+	texture_progress_bar.value = playerStatsComponent.exp
+	texture_progress_bar.max_value = playerStatsComponent.max_exp	
+	texture_progress_bar.min_value = 0
+	playerStatsComponent.exp_changed.connect( func():
+		set_texture_progress()
+	)
+	set_texture_progress()
+	
+func set_texture_progress():
+	texture_progress_bar.value = playerStatsComponent.exp
+
+
+
+
--- scenes/components/stats/player_stats_component.gd
+++ scenes/components/stats/player_stats_component.gd
@@ -1,8 +1,25 @@
 class_name PlayerStatsComponent
 extends ActorStatsComponent
 
-var exp = 1
 var pstats: PlayerStats
 func _ready():
+	super()
 	pstats = stats
 	exp = pstats.exp
+	
+
+# Create the health variable and connect a setter
+var max_exp: int = 100
+var exp: int = 0:
+	set(value):
+		exp = value
+		
+		# Signal out that the exp has changed
+		exp_changed.emit()
+		
+		# Signal out when exp is at 0
+		if exp >= max_exp: level_up.emit()
+
+# Create our signals for exp
+signal exp_changed() # Emit when the exp value has changed
+signal level_up() # Emit when there is no health left
