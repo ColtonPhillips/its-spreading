41fd545fb66c995cb5fdd1bf490a6615f7eb331c
Colton Phillips <coltonjphillips@gmail.com>
2024-02-20 12:04:37 -0800

    add fps warning

--- scenes/components/exp_bar_component/exp_bar_component.gd
+++ scenes/components/exp_bar_component/exp_bar_component.gd
@@ -1,12 +1,14 @@
 class_name ExpBarComponent
 extends Node
 
+@onready var fps_low_on_mobile_label: Label = $FPSLowOnMobileLabel
 @onready var texture_progress_bar: TextureProgressBar = $TextureProgressBar
 var playerStatsComponent: PlayerStatsComponent
 @onready var fps_meter: Label = $FPSMeter
 @export var fps_meter_enabled := false
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	fps_low_on_mobile_label.visible = false
 	playerStatsComponent = Global.player.stats_component
 	texture_progress_bar.value = playerStatsComponent.exp
 	texture_progress_bar.max_value = playerStatsComponent.max_exp	
@@ -22,8 +24,19 @@ func _ready():
 func set_texture_progress():
 	texture_progress_bar.value = playerStatsComponent.exp
 
+var has_flashed_message_once = false
 func _process(delta):
+	if not has_flashed_message_once and not OS.has_feature("pc"):
+		if Engine.get_frames_per_second() < 12:
+			fps_low_on_mobile_label.visible = true
+			disable_that_thang()
+		else:
+			fps_low_on_mobile_label.visible = false
 	if (fps_meter_enabled):
 		fps_meter.set_text(str( Engine.get_frames_per_second()))
+func disable_that_thang():
+	has_flashed_message_once = true
+	await get_tree().create_timer(8.0).timeout
+	fps_low_on_mobile_label.visible = false
 
 
