3a49bf3d4e45242b8c224660570bdfe22c745378
Colton Phillips <coltonjphillips@gmail.com>
2024-03-01 19:44:41 -0800

    fixed position problem

--- scenes/gui/story/story_menu.gd
+++ scenes/gui/story/story_menu.gd
@@ -1,16 +1,16 @@
 class_name StoryMenu
 extends Node2D
-@onready var label:Label = $Label
+@onready var node_2d: Node2D = $Node2D
+
 @export var end_pos: int
 @export var scroll_speed: float
 func _ready():
-	label.position.snapped(Vector2.ONE)
+	node_2d.position.snapped(Vector2.ONE)
 	pass
 
 func _physics_process(delta):
-	if (label.position.y > end_pos):
-		label.position.y -= scroll_speed
-
+	if (node_2d.position.y > end_pos):
+		node_2d.position.y -= scroll_speed
 
 func _on_link_button_pressed():
 	get_tree().change_scene_to_file('res://scenes/gui/main_menu/main_menu.tscn')
